580eccd25de291e2cbe4a15e912af06ed04e084c
Kendyl Landeck <46770740+Kendyl66@users.noreply.github.com>
2019-03-16 10:48:02 -0500

    Practicing with help

--- ex8/universities.js
+++ ex8/universities.js
@@ -1,1 +1,305 @@
-{"type":"FeatureCollection","features":[{"type":"Feature","properties":{"description":"1. Princeton University","Acceptance Rate":"6%"},"geometry":{"type":"Point","coordinates":[-74.656111,40.345278]}},{"type":"Feature","properties":{"description":"2. Harvard Univeristy","Acceptance Rate":"5%"},"geometry":{"type":"Point","coordinates":[-71.116944,42.374444]}},{"type":"Feature","properties":{"description":"3. Columbia University","Acceptance Rate":"7%"},"geometry":{"type":"Point","coordinates":[-73.961944,40.8075]}},{"type":"Feature","properties":{"description":"4. Massachusetts Instutute of Technology","Acceptance Rate":"7%"},"geometry":{"type":"Point","coordinates":[-71.092,42.36]}},{"type":"Feature","properties":{"description":"5. University of Chicago","Acceptance Rate":"8%"},"geometry":{"type":"Point","coordinates":[-87.599722,41.789722]}},{"type":"Feature","properties":{"description":"6.Yale University","Acceptance Rate":"7%"},"geometry":{"type":"Point","coordinates":[-72.926667,41.311111]}},{"type":"Feature","properties":{"description":"7. Standofrd University","Acceptance Rate":"5%"},"geometry":{"type":"Point","coordinates":[-122.168858,37.428229]}},{"type":"Feature","properties":{"description":"8. Duke University","Acceptance Rate":"10%"},"geometry":{"type":"Point","coordinates":[-78.938889,36.001111]}},{"type":"Feature","properties":{"description":"9. University of Pennsylvania","Acceptance Rate":"9%"},"geometry":{"type":"Point","coordinates":[-75.19,39.95]}},{"type":"Feature","properties":{"description":"10. Johns Hopkins University","Acceptance Rate":"13%"},"geometry":{"type":"Point","coordinates":[-76.620278,39.328889]}},{"type":"Feature","properties":{"description":"11. Northwestern University","Acceptance Rate":"9%"},"geometry":{"type":"Point","coordinates":[-87.673945,42.054853]}},{"type":"Feature","properties":{"description":"12. California Institute of Technology","Acceptance Rate":"8%"},"geometry":{"type":"Point","coordinates":[-118.125,34.138]}},{"type":"Feature","properties":{"description":"13. Dartmouth College","Acceptance Rate":"10%"},"geometry":{"type":"Point","coordinates":[-72.288333,43.703333]}},{"type":"Feature","properties":{"description":"14. Brown University","Acceptance Rate":"8%"},"geometry":{"type":"Point","coordinates":[-71.4032,41.8262]}},{"type":"Feature","properties":{"description":"15. Vanderbilt University","Acceptance Rate":"11%"},"geometry":{"type":"Point","coordinates":[-86.804972,36.148649]}},{"type":"Feature","properties":{"description":"16. Cornell Univeristy","Acceptance Rate":"13%"},"geometry":{"type":"Point","coordinates":[-76.483056,42.447222]}},{"type":"Feature","properties":{"description":"17. Rice University","Acceptance Rate":"16%"},"geometry":{"type":"Point","coordinates":[-95.402778,29.716944]}},{"type":"Feature","properties":{"description":"18. University of Notre Dame","Acceptance Rate":"19%"},"geometry":{"type":"Point","coordinates":[-86.239,41.703]}},{"type":"Feature","properties":{"description":"19. University of California- Los Angeles","Acceptance Rate":"16%"},"geometry":{"type":"Point","coordinates":[-118.445278,34.069444]}},{"type":"Feature","properties":{"description":"20. Washington University","Acceptance Rate":"16%"},"geometry":{"type":"Point","coordinates":[-90.305,38.648]}}]}
+
+{
+	"type": "FeatureCollection",
+	"features": [{
+		"type": "Feature",
+		"properties": {
+			"description": "Princeton University",
+			"marker-color": "#ff0000",
+			"marker-symbol": "college",
+			"number": 3,
+			"Ranking": 1,
+			"acceptancerate": "6%",
+			"marker-size": "medium"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-74.656111, 40.345278]
+		}
+	}, {
+		"type": "Feature",
+		"properties": {
+			"description": "Harvard Univeristy",
+			"marker-color": "#ff0000",
+			"marker-symbol": "college",
+			"number": 3,
+			"Ranking": 2,
+			"acceptancerate": "5%",
+			"marker-size": "medium"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-71.116944, 42.374444]
+		}
+	}, {
+		"type": "Feature",
+		"properties": {
+			"description": "Columbia University",
+			"marker-color": "#ff0000",
+			"marker-symbol": "college",
+			"number": 3,
+			"Ranking": 3,
+			"acceptancerate": "7%",
+			"marker-size": "medium"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-73.961944, 40.8075]
+		}
+	}, {
+		"type": "Feature",
+		"properties": {
+			"description": "Massachusetts Instutute of Technology",
+			"marker-color": "#ff0000",
+			"marker-symbol": "college",
+			"number": 3,
+			"Ranking": 4,
+			"acceptancerate": "7%",
+			"marker-size": "medium"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-71.092, 42.36]
+		}
+	}, {
+		"type": "Feature",
+		"properties": {
+			"marker-size": "medium",
+			"description": "University of Chicago",
+			"marker-color": "#ff0000",
+			"marker-symbol": "college",
+			"number": 3,
+			"Ranking": 5,
+			"acceptancerate": "8%"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-87.599722, 41.789722]
+		}
+	}, {
+		"type": "Feature",
+		"properties": {
+			"description": "Yale University",
+			"marker-color": "#ff0000",
+			"marker-symbol": "college",
+			"number": 3,
+			"Ranking": 6,
+			"acceptancerate": "7%",
+			"marker-size": "medium"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-72.926667, 41.311111]
+		}
+	}, {
+		"type": "Feature",
+		"properties": {
+			"marker-color": "#ff0000",
+			"marker-size": "medium",
+			"marker-symbol": "college",
+			"description": "Standofrd University",
+			"number": 3,
+			"Ranking": 7,
+			"acceptancerate": "5%"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-122.168858, 37.428229]
+		}
+	}, {
+		"type": "Feature",
+		"properties": {
+			"description": "Duke University",
+			"marker-symbol": "college",
+			"marker-color": "#feec12",
+			"number": 2,
+			"Ranking": 8,
+			"acceptancerate": "10%",
+			"marker-size": "medium"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-78.938889, 36.001111]
+		}
+	}, {
+		"type": "Feature",
+		"properties": {
+			"description": "University of Pennsylvania",
+			"marker-symbol": "college",
+			"marker-color": "#feec12",
+			"number": 2,
+			"Ranking": 9,
+			"acceptancerate": "9%",
+			"marker-size": "medium"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-75.19, 39.95]
+		}
+	}, {
+		"type": "Feature",
+		"properties": {
+			"description": "Johns Hopkins University",
+			"marker-symbol": "college",
+			"marker-color": "#feec12",
+			"number": 2,
+			"Ranking": 10,
+			"acceptancerate": "13%",
+			"marker-size": "medium"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-76.620278, 39.328889]
+		}
+	}, {
+		"type": "Feature",
+		"properties": {
+			"description": "Northwestern University",
+			"marker-symbol": "college",
+			"marker-color": "#feec12",
+			"number": 2,
+			"Ranking": 11,
+			"acceptancerate": "9%",
+			"marker-size": "medium"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-87.673945, 42.054853]
+		}
+	}, {
+		"type": "Feature",
+		"properties": {
+			"description": "California Institute of Technology",
+			"marker-symbol": "college",
+			"marker-color": "#ff0000",
+			"number": 3,
+			"Ranking": 12,
+			"acceptancerate": "8%",
+			"marker-size": "medium"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-118.125, 34.138]
+		}
+	}, {
+		"type": "Feature",
+		"properties": {
+			"description": "Dartmouth College",
+			"marker-symbol": "college",
+			"marker-color": "#feec12",
+			"number": 2,
+			"Ranking": 13,
+			"acceptancerate": "10%",
+			"marker-size": "medium"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-72.288333, 43.703333]
+		}
+	}, {
+		"type": "Feature",
+		"properties": {
+			"description": "Brown University",
+			"marker-symbol": "college",
+			"marker-color": "#ff0000",
+			"number": 3,
+			"Ranking": 14,
+			"acceptancerate": "8%",
+			"marker-size": "medium"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-71.4032, 41.8262]
+		}
+	}, {
+		"type": "Feature",
+		"properties": {
+			"description": "Vanderbilt University",
+			"marker-symbol": "college",
+			"marker-color": "#feec12",
+			"number": 2,
+			"Ranking": 15,
+			"acceptancerate": "11%",
+			"marker-size": "medium"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-86.804972, 36.148649]
+		}
+	}, {
+		"type": "Feature",
+		"properties": {
+			"description": "Cornell Univeristy",
+			"marker-symbol": "college",
+			"marker-color": "#feec12",
+			"number": 2,
+			"Ranking": 16,
+			"acceptancerate": "13%",
+			"marker-size": "medium"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-76.483056, 42.447222]
+		}
+	}, {
+		"type": "Feature",
+		"properties": {
+			"description": "Rice University",
+			"marker-color": "#00d700",
+			"marker-symbol": "college",
+			"number": 1,
+			"Ranking": 17,
+			"acceptancerate": "16%",
+			"marker-size": "medium"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-95.402778, 29.716944]
+		}
+	}, {
+		"type": "Feature",
+		"properties": {
+			"description": "University of Notre Dame",
+			"marker-color": "#00d700",
+			"marker-symbol": "college",
+			"number": 1,
+			"Ranking": 18,
+			"acceptancerate": "19%",
+			"marker-size": "medium"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-86.239, 41.703]
+		}
+	}, {
+		"type": "Feature",
+		"properties": {
+			"marker-color": "#00d700",
+			"marker-size": "medium",
+			"marker-symbol": "college",
+			"description": "University of California- Los Angeles",
+			"number": 1,
+			"Ranking": 19,
+			"acceptancerate": "16%"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-118.445278, 34.069444]
+		}
+	}, {
+		"type": "Feature",
+		"properties": {
+			"description": "Washington University",
+			"marker-color": "#00d700",
+			"marker-symbol": "college",
+			"number": 1,
+			"Ranking": 20,
+			"acceptancerate": "16%",
+			"marker-size": "medium"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-90.305, 38.648]
+		}
+	}]
+}
