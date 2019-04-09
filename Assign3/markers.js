e5a76a490eab1fb66ca348c1212ea4e0b64c9427
Kendyl Landeck <46770740+Kendyl66@users.noreply.github.com>
2019-04-09 17:29:52 -0500

    update

--- Assign3/markers.js
+++ Assign3/markers.js
@@ -1,10 +1,9 @@
 console.clear();
-
 var map = L.map('map').setView([48.2, 16.366667], 12);
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-  attribution: 'ISUEO GIS',
-  maxZoom: 18
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+    maxZoom: 18
 }).addTo(map);
 
 //font Awesome possible colors are as follows - no yellow?
@@ -14,10 +13,9 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 var marker1 = L.marker([42.0, -93.09], {
 		icon: L.AwesomeMarkers.icon({
 			icon: 'flag', 
-			prefix: 'fa', //from fonts awesome
+			prefix:'fa', //from fonts awesome
 			markerColor: 'green',
 			iconColor: 'yellow',
 			spin: true
 		}) 
 });
-</html>
