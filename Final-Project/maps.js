1b226bcaea3c71f3f7a3677c485c214db8704013
Kendyl Landeck <46770740+Kendyl66@users.noreply.github.com>
2019-05-04 09:04:39 -0500

    final project updates

--- Final-Project/maps.js
+++ Final-Project/maps.js
@@ -1,6 +1,6 @@
-      var osm = L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+var osm = L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-});
+      });
         var esriWorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
 	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 });
@@ -9,18 +9,18 @@
                            layers: [osm]
                            });
   // need to put in colors depending on percent value for each map, so I will have 
-    function getColor40 (d){
-        return d > 20 ? '#b10026':
-        d > 10 ? '#f03b20':
-        d > 5 ? '#fd8d3c':
-        d > 1 ? '#fecc5c':
-        d > 0 ?'#ffffcc':
+    function getColor(d) {
+        return d > 20 ? '#b10026' :
+        d > 10 ? '#f03b20' :
+        d > 5 ? '#fd8d3c' :
+        d > 1 ? '#fecc5c' :
+        d > 0 ?'#ffffcc' :
         '#FFFFFF'; //this will be the default if none of the above match these numbers
     }
     //now i need to set the function for style
         function style(feature) {
             return {
-                fillColor: getColor40(feature.properties.Percent_To * 100),
+                fillColor: getColor(feature.properties.Percent_To * 100),
                 stroke: true,
                 weight: 2,
                 color: '#000000',
@@ -29,43 +29,20 @@
                 //dashArray: '3'
             };
         }
-    function highlightFeature(e) {
-        var layer = e.target;
-        layer.setStyle({
-            weight: 5,
-            color: '#666',
-            dashArray:'',
-            fillOpacity:0.7
-        });
-        if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {layer.bringToFront();
-        }
-    }
-        function resetHighlight(e) {geojsonLayer.resetStyle(e.target);
-        }
-        
-    function zoomToFeature(e) {
-        map.fitBounds(e.target.getBounds());
-        }
-    function onEachFeature(feature, layer) {
-    layer.on({
-        mouseover: highlightFeature,
-        mouseout: resetHighlight,
-        click: zoomToFeature
-    });
-}
-
-function getColor70 (d){
-        return d > 20 ? '#b10026':
-        d > 10 ? '#f03b20':
-        d > 5 ? '#fd8d3c':
-        d > 1 ? '#fecc5c':
-        d > 0 ?'#ffffcc':
+          
+          
+function getColor2(d) {
+        return d > 20 ? '#b10026' :
+        d > 10 ? '#f03b20' :
+        d > 5 ? '#fd8d3c' :
+        d > 1 ? '#fecc5c' :
+        d > 0 ?'#ffffcc' :
         '#FFFFFF'; //this will be the default if none of the above match these numbers
     }
     //now i need to set the function for style
         function style2(feature) {
             return {
-                fillColor: getColor70(feature.properties.PerTot * 100),
+                fillColor: getColor2(feature.properties.PerTot * 100),
                 stroke: true,
                 weight: 2,
                 color: '#000000',
@@ -74,72 +51,66 @@ function getColor70 (d){
                 //dashArray: '3'
             };
         }
-
-   
-// loading in each different legend
-var legend1 = L.control({ 
-    position: 'bottomleft'});
-     
-legend1.onAdd = function (map) {
-        var div = L.DomUtil.create('div', 'info legend'); 
-     var breaks = [1,2,6,11,30];
-    var labels = ['0%', '1%-5%', '6%-10%', '11%-20%','21% and Greater'];
-           
-        div.innerHTML += "<b> African American Population</b> <br>"
-         
-    for (var i = 0; i <breaks.length; i++){ 
-    div.innerHTML +=
-        '<i style="background:' + getColor40(breaks[i]) + ' "></i> ' + labels[i] + (breaks ? '' + '<br>' : '');
+function getColor3(d) {
+        return d > 20 ? '#b10026' :
+        d > 10 ? '#f03b20' :
+        d > 5 ? '#fd8d3c' :
+        d > 1 ? '#fecc5c' :
+        d > 0 ?'#ffffcc' :
+        '#FFFFFF'; //this will be the default if none of the above match these numbers
+    }
+    //now i need to set the function for style
+        function style3(feature) {
+            return {
+                fillColor: getColor3(feature.properties.PERCB * 100),
+                stroke: true,
+                weight: 2,
+                color: '#000000',
+                opacity: 1.0,
+                fillOpacity: 0.6, 
+                //dashArray: '3'
+            };
         }
-    return div;
-     };
-
-var legend2 = L.control({ 
-    position: 'bottomleft'});
-     
-legend2.onAdd = function (map) {
-        var div = L.DomUtil.create('div', 'info legend'); 
-     var breaks = [1,2,6,11,30];
-    var labels = ['0%', '1%-5%', '6%-10%', '11%-20%','21% and Greater'];
-           
-        div.innerHTML += "<b> African American Population</b> <br>"
-         
-    for (var i = 0; i <breaks.length; i++){ 
-    div.innerHTML +=
-        '<i style="background:' + getColor70(breaks[i]) + ' "></i> ' + labels[i] + (breaks ? '' + '<br>' : '');
-        }
-    return div;
-     };
-
+function getColor4(d) {
+        return d > 20 ? '#b10026' :
+        d > 10 ? '#f03b20' :
+        d > 5 ? '#fd8d3c' :
+        d > 1 ? '#fecc5c' :
+        d > 0 ?'#ffffcc' :
+        '#FFFFFF'; //this will be the default if none of the above match these numbers
+    }
+    //now i need to set the function for style
+        function style4(feature) {
+            return {
+                fillColor: getColor4(feature.properties.PERCB * 100),
+                stroke: true,
+                weight: 2,
+                color: '#000000',
+                opacity: 1.0,
+                fillOpacity: 0.6, 
+                //dashArray: '3'
+            };
+        };
+ 
 //Load GeoJSON files for each year
 var geojsonLayer = new L.GeoJSON.AJAX("https://kendyl66.github.io/LA458-558/Final-Project/1940.geojson", {
   style: style,
-  onEachFeature: onEachFeature
 });
 
 geojsonLayer.addTo(map);
 
 //Load GeoJSON
 var geojsonLayer2 = new L.GeoJSON.AJAX("https://kendyl66.github.io/LA458-558/Final-Project/1970.geojson", {
-  style2: style,
-  onEachFeature: onEachFeature
+ style: style2,
 });
-
-geojsonLayer.addTo(map);
     
 var geojsonLayer3 = new L.GeoJSON.AJAX("https://kendyl66.github.io/LA458-558/Final-Project/2000.geojson", {
-  style: style,
-  onEachFeature: onEachFeature
+style: style3,
 });
-
-geojsonLayer.addTo(map);
 
 var geojsonLayer4 = new L.GeoJSON.AJAX("https://kendyl66.github.io/LA458-558/Final-Project/2010.geojson", {
-  style: style,
-  onEachFeature: onEachFeature
+style: style4,
 });
-
-geojsonLayer.addTo(map);
 
 
         var baseMaps = {
@@ -150,26 +121,29 @@ geojsonLayer.addTo(map);
             "1940": geojsonLayer,
             "1970": geojsonLayer2,
             "2000": geojsonLayer3,
-            "2010": geojsonLayer4
+            "2010": geojsonLayer4,  
         };
           
     L.control.layers(baseMaps, overlayMaps).addTo(map);
-        
+   
+    // loading in each different legend
 var legend1 = L.control({ 
     position: 'bottomleft'});
      
 legend1.onAdd = function (map) {
         var div = L.DomUtil.create('div', 'info legend'); 
      var breaks = [1,2,6,11,30];
-    var labels = ['0%', '1%-5%', '6%-10%', '11%-20%','21% and Greater'];
+    var labels = ['0%', '1%-5%', '6%-10%', '11%-20%','21% and Greater'];    
            
-        div.innerHTML += "<b> African American Population</b> <br>"
+        div.innerHTML += "<b>African American Population</b> <br>"
          
     for (var i = 0; i <breaks.length; i++){ 
     div.innerHTML +=
-        '<i style="background:' + getColor40(breaks[i]) + ' "></i> ' + labels[i] + (breaks ? '' + '<br>' : '');
+        '<i style="background:' + getColor(breaks[i]) + ' "></i> ' + labels[i] + (breaks ? '' + '<br>' : '');
         }
     return div;
      };
+          
+
+          
      legend1.addTo(map);
- 
