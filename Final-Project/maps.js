5abe8b31f067ea4883cc7edb18f4657c22103d92
Kendyl Landeck <46770740+Kendyl66@users.noreply.github.com>
2019-05-03 22:18:38 -0500

    Update maps.js

--- Final-Project/maps.js
+++ Final-Project/maps.js
@@ -8,7 +8,7 @@
                            zoom: 11,
                            layers: [osm]
                            });
-  // need to put in colors depending on percent value
+  // need to put in colors depending on percent value for each map, so I will have 
     function getColor40 (d){
         return d > 20 ? '#b10026':
         d > 10 ? '#f03b20':
@@ -54,24 +54,103 @@
     });
 }
 
-    
-        //Loading GeoJSON
+function getColor70 (d){
+        return d > 20 ? '#b10026':
+        d > 10 ? '#f03b20':
+        d > 5 ? '#fd8d3c':
+        d > 1 ? '#fecc5c':
+        d > 0 ?'#ffffcc':
+        '#FFFFFF'; //this will be the default if none of the above match these numbers
+    }
+    //now i need to set the function for style
+        function style2(feature) {
+            return {
+                fillColor: getColor70(feature.properties.PerTot * 100),
+                stroke: true,
+                weight: 2,
+                color: '#000000',
+                opacity: 1.0,
+                fillOpacity: 0.6, 
+                //dashArray: '3'
+            };
+        }
 
-//Load GeoJSON
+   
+// loading in each different legend
+var legend1 = L.control({ 
+    position: 'bottomleft'});
+     
+legend1.onAdd = function (map) {
+        var div = L.DomUtil.create('div', 'info legend'); 
+     var breaks = [1,2,6,11,30];
+    var labels = ['0%', '1%-5%', '6%-10%', '11%-20%','21% and Greater'];
+           
+        div.innerHTML += "<b> African American Population</b> <br>"
+         
+    for (var i = 0; i <breaks.length; i++){ 
+    div.innerHTML +=
+        '<i style="background:' + getColor40(breaks[i]) + ' "></i> ' + labels[i] + (breaks ? '' + '<br>' : '');
+        }
+    return div;
+     };
+
+var legend2 = L.control({ 
+    position: 'bottomleft'});
+     
+legend2.onAdd = function (map) {
+        var div = L.DomUtil.create('div', 'info legend'); 
+     var breaks = [1,2,6,11,30];
+    var labels = ['0%', '1%-5%', '6%-10%', '11%-20%','21% and Greater'];
+           
+        div.innerHTML += "<b> African American Population</b> <br>"
+         
+    for (var i = 0; i <breaks.length; i++){ 
+    div.innerHTML +=
+        '<i style="background:' + getColor70(breaks[i]) + ' "></i> ' + labels[i] + (breaks ? '' + '<br>' : '');
+        }
+    return div;
+     };
+
+//Load GeoJSON files for each year
 var geojsonLayer = new L.GeoJSON.AJAX("https://kendyl66.github.io/LA458-558/Final-Project/1940.geojson", {
   style: style,
   onEachFeature: onEachFeature
 });
 
 geojsonLayer.addTo(map);
+
+//Load GeoJSON
+var geojsonLayer2 = new L.GeoJSON.AJAX("https://kendyl66.github.io/LA458-558/Final-Project/1970.geojson", {
+  style2: style,
+  onEachFeature: onEachFeature
+});
+
+geojsonLayer.addTo(map);
     
-        
+var geojsonLayer3 = new L.GeoJSON.AJAX("https://kendyl66.github.io/LA458-558/Final-Project/2000.geojson", {
+  style: style,
+  onEachFeature: onEachFeature
+});
+
+geojsonLayer.addTo(map);
+
+var geojsonLayer4 = new L.GeoJSON.AJAX("https://kendyl66.github.io/LA458-558/Final-Project/2010.geojson", {
+  style: style,
+  onEachFeature: onEachFeature
+});
+
+geojsonLayer.addTo(map);
+
+
         var baseMaps = {
         "OpenStreetMap": osm,
         "Satellite View": esriWorldImagery,
     };
         var overlayMaps = {
-            "1940": geojsonLayer
+            "1940": geojsonLayer,
+            "1970": geojsonLayer2,
+            "2000": geojsonLayer3,
+            "2010": geojsonLayer4
         };
           
     L.control.layers(baseMaps, overlayMaps).addTo(map);
